7558c65966289a307f7d39b737858ed5f92c75f7
Maxim-Borisov7 <maxvadbor@mail.ru>
2024-07-21 16:31:28 +0500

    fixed error

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ class Circle:
         self.radius = radius
 
     def are_circle(self, radius):
-        return round(math.pi * (radius * radius), 2)
+        return round(math.pi * (radius ** 2), 2)
 
 class Triangle:
     def __init__(self, a, b, c):
@@ -12,11 +12,10 @@ class Triangle:
         self.b = b
         self.c = c
     def check_triangle(self, a, b, c):
-        if a == b + c or b == a + c or c == a + b:
+        if a ** 2 == b ** 2 + c ** 2 or b ** 2 == a ** 2 + c ** 2 or c ** 2 == a ** 2 + b ** 2:
             return True
         return False
     def area_triangle(self, a, b, c):
         p = (a + b + c) / 2
         return round(math.sqrt(p * ((p - a) * (p - b) * (p - c))), 2)
 
-print('Hello world')
